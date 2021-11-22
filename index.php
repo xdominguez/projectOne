19ba3c008c38efe1edab346a4a4dcd1a66caf3e1
xdominguez <xavierdominguezb@hotmail.com>
2021-11-21 22:47:43 -0500

    Cambios de pruebas

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prueba de github</title>
     <h1>
-        hola Mundo
+        hola Mundo, hola a todos
 </h1>
 </head>
 <body>
